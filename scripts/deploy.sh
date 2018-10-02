c19ba5451438b8563626b17db1bfd78c1a72977d
Nathan Toups <n@rjrbt.io>
2018-10-01 23:09:07 -0600

    POC

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ DIST_ID=$HASHMAP_CDN_DIST_ID
 CACHE_ID=`date +%s`
 FORMATTED_JSON='{"Paths": {"Quantity": 1,"Items": ["/*"]},"CallerReference":"'${CACHE_ID}'"}'
 
-npm run build
+npm run build:prod
 cd $DIR
 aws s3 sync . $BUCKET --acl public-read
 aws cloudfront create-invalidation --distribution-id $DIST_ID --invalidation-batch "$FORMATTED_JSON"
